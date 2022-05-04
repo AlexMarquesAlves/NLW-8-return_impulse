f69d7e973bd81bd62a036b780dbbeb33c07756f8
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-03 23:36:14 -0300

    Feat: new span

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -3,9 +3,11 @@ import { ChatTeardropDots } from "phosphor-react";
 export function Widget() {
   return (
     <div className="absolute bottom-5 right-5">
-      <button className="h-12 px-3 text-white rounded-full bg-brand-500">
+      <button className="flex items-center h-12 px-3 text-white rounded-full bg-brand-500">
         <ChatTeardropDots className="w-6 h-6" />
       </button>
+
+      <span>Feedback</span>
     </div>
   );
 }
