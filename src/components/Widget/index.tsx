9779b6934a062804fd6d99b005f4b32b84fb6113
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-03 23:55:06 -0300

    Feat: use headless ui to a11y

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,28 +1,19 @@
+import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen);
-  }
-
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpen && <p>Hello World!</p>}
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hello World!</Popover.Panel>
 
-      <button
-        onClick={toggleWidgetVisibility}
-        className="flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group "
-      >
+      <Popover.Button className="flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group ">
         <ChatTeardropDots className="w-6 h-6" />
-      </button>
+      </Popover.Button>
 
       <span className="overflow-hidden transition-all duration-500 ease-linear max-w-0 group-hover:max-w-xs">
         <span className="pl-2"></span>
         Feedback
       </span>
-    </div>
+    </Popover>
   );
 }
