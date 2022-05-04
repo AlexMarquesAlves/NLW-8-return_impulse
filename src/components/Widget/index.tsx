55c6b193c8667a60b920fd6a8516e157280c7d9e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-03 23:33:18 -0300

    Feat: new button

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -2,8 +2,8 @@ import { ChatTeardropDots } from "phosphor-react";
 
 export function Widget() {
   return (
-    <button>
-      <ChatTeardropDots />
+    <button className="h-12 px-3 text-white rounded-full bg-brand-500">
+      <ChatTeardropDots className="w-6 h-6" />
     </button>
   );
 }
