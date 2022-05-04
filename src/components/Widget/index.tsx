f96809b128df2a60839ba49522b3a225bbffdaae
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-03 23:40:36 -0300

    Feat: coding animation of hover button

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -3,11 +3,14 @@ import { ChatTeardropDots } from "phosphor-react";
 export function Widget() {
   return (
     <div className="absolute bottom-5 right-5">
-      <button className="flex items-center h-12 px-3 text-white rounded-full bg-brand-500">
+      <button className="flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group ">
         <ChatTeardropDots className="w-6 h-6" />
       </button>
 
-      <span>Feedback</span>
+      <span className="overflow-hidden transition-all duration-500 ease-linear max-w-0 group-hover:max-w-xs">
+        <span className="pl-2"></span>
+        Feedback
+      </span>
     </div>
   );
 }
