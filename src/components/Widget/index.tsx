7a7621c734d41ab24b4c4aaebefba049b53ebfaa
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-03 23:43:45 -0300

    Feat: contolling state of button with useState

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,8 +1,13 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
   return (
     <div className="absolute bottom-5 right-5">
+      <p>Hello World!</p>
+
       <button className="flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group ">
         <ChatTeardropDots className="w-6 h-6" />
       </button>
