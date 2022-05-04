dba966bedf1fe59df38b52afe55cb55351d3c021
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-03 23:34:44 -0300

    Feat: new div to move the position of button

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -2,8 +2,10 @@ import { ChatTeardropDots } from "phosphor-react";
 
 export function Widget() {
   return (
-    <button className="h-12 px-3 text-white rounded-full bg-brand-500">
-      <ChatTeardropDots className="w-6 h-6" />
-    </button>
+    <div className="absolute bottom-5 right-5">
+      <button className="h-12 px-3 text-white rounded-full bg-brand-500">
+        <ChatTeardropDots className="w-6 h-6" />
+      </button>
+    </div>
   );
 }
