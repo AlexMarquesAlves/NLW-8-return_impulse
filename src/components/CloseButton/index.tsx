62b9743182abc802ca2324efab78a0408f748b8b
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-12 00:40:38 -0300

    Feat: update CloseButton

--- src/components/CloseButton/index.tsx
+++ src/components/CloseButton/index.tsx
@@ -4,10 +4,10 @@ import { X } from "phosphor-react";
 export function CloseButton() {
   return (
     <Popover.Button
-      className={"top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"}
-      title={"Fechar formulário de feedback"}
+      className="top-5 right-4 rounded-md border-transparent absolute text-zinc-500 hover:text-zinc-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+      title="Fechar formulário de feedback"
     >
-      <X weight="bold" className={"w-4 h-4"} />
+      <X weight="bold" className="w-4 h-4" />
     </Popover.Button>
   );
 }
