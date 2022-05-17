2c7179f8b612fee62c8be2c8dcdb4c8c98250ae9
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-16 23:24:06 -0300

    Feat: implement Dependency Inversion Principle

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import nodemailer from "nodemailer";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
@@ -16,20 +18,29 @@ const transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = "";
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  );
 
-  await transport.sendMail({
-    from: `Feedget team <oi@feedget.com>`,
-    to: `Alex Marques Alves <atanaelleonardo@gmail.com>`,
-    subject: `Novo Feedback`,
-    html: [
-      `<div style='font-family:sans-serif; font-size:16px; color:#111; ' >`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `<p>Screenshot: ${screenshot}</p>`,
-      `</div>`,
-    ].join("\n"),
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
   });
 
-  return res.status(201).json({ data: feedback });
+  // await transport.sendMail({
+  //   from: `Feedget team <oi@feedget.com>`,
+  //   to: `Alex Marques Alves <atanaelleonardo@gmail.com>`,
+  //   subject: `Novo Feedback`,
+  //   html: [
+  //     `<div style='font-family:sans-serif; font-size:16px; color:#111; ' >`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `<p>Screenshot: ${screenshot}</p>`,
+  //     `</div>`,
+  //   ].join("\n"),
+  // });
+
+  return res.status(201).send();
 });
