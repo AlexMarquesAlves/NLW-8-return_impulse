193fd54717e366e8ae78bb2abaee6d5fcf636d46
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-16 21:58:45 -0300

    Feat: Nodemailer transport

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
 
+// MAIL PROVIDER
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b26234ed7ccf61",
+    pass: "49054c5f45e8ab",
+  },
+});
+
+// FEEDBACKS ROUTE
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
