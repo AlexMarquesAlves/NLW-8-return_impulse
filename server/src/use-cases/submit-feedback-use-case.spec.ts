0a906ff0a70fb0a922bfdc629f0b4c878e0d62e2
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-17 13:45:30 -0300

    feat: Submit feedback jest test

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,18 @@
-test("should sum 2 + 2", () => {
-  expect(2 + 2).toBe(4);
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+describe("Submit feedback", () => {
+  it("Should be able to submit a feedback", async () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: async () => {} },
+      { sendMail: async () => {} }
+    );
+
+    await expect(
+      submitFeedback.execute({
+        type: `Bug`,
+        comment: `comment example`,
+        screenshot: `example.jpg`,
+      })
+    ).resolves.not.toThrow();
+  });
 });
