b462deee6deb55a11534cc813d438adf69fea5d6
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-16 23:45:58 -0300

    Feat: implement Dependency Inversion Principle on sendMail

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,26 +1,19 @@
 import express from "express";
-import nodemailer from "nodemailer";
+import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
 import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
-// MAIL PROVIDER
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "b26234ed7ccf61",
-    pass: "49054c5f45e8ab",
-  },
-});
-
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
   );
 
   await submitFeedbackUseCase.execute({
@@ -28,19 +21,6 @@ routes.post("/feedbacks", async (req, res) => {
     comment,
     screenshot,
   });
-
-  // await transport.sendMail({
-  //   from: `Feedget team <oi@feedget.com>`,
-  //   to: `Alex Marques Alves <atanaelleonardo@gmail.com>`,
-  //   subject: `Novo Feedback`,
-  //   html: [
-  //     `<div style='font-family:sans-serif; font-size:16px; color:#111; ' >`,
-  //     `<p>Tipo do feedback: ${type}</p>`,
-  //     `<p>Comentário: ${comment}</p>`,
-  //     `<p>Screenshot: ${screenshot}</p>`,
-  //     `</div>`,
-  //   ].join("\n"),
-  // });
 
   return res.status(201).send();
 });
