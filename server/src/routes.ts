1549ca706edb87fc77aa6b12fc50b15ec190cbbd
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-16 22:35:42 -0300

    Feat: Separate routes

---
+++ server/src/routes.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
+
+export const routes = express.Router();
+
+// MAIL PROVIDER
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b26234ed7ccf61",
+    pass: "49054c5f45e8ab",
+  },
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: `Feedget team <oi@feedget.com>`,
+    to: `Alex Marques Alves <atanaelleonardo@gmail.com>`,
+    subject: `Novo Feedback`,
+    html: [
+      `<div style='font-family:sans-serif; font-size:16px; color:#111; ' >`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `<p>Screenshot: ${screenshot}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
+});
