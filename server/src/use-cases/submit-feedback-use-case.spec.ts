372c86f834731ae231482842ffb2b0e0de9f22b3
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-17 23:19:31 -0300

    Jest: Submit feedback test without type

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,12 +1,13 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
-describe("Submit feedback", () => {
-  it("Should be able to submit a feedback", async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    );
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {} },
+  { sendMail: async () => {} }
+);
 
+describe("Submit feedback", () => {
+  // submit a feedback it's working well
+  it("Should be able to submit a feedback", async () => {
     await expect(
       submitFeedback.execute({
         type: `Bug`,
@@ -14,5 +15,16 @@ describe("Submit feedback", () => {
         screenshot: `data:image/png;base64,12431j414l1h4`,
       })
     ).resolves.not.toThrow();
+  });
+
+  // without type
+  it("Should not be able to submit a feedback without type", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: ``,
+        comment: `comment example`,
+        screenshot: `data:image/png;base64,12431j414l1h4`,
+      })
+    ).rejects.toThrow();
   });
 });
